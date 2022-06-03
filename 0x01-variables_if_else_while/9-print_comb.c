b3bc142c52d66b8b45e3b7e538fe167b9180bb6f
Tevstark <tevkamau@gmail.com>
2022-06-03 12:43:06 -0700

    task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+*main - prints possible 0-9 combination
+*
+*Return: 0
+*/
+int main(void)
+{
+int i;
+for (i = '0'; i <= '9'; i++)
+{
+putchar(i);
+if (i == '9')
+break;
+putchar(',');
+putchar(' ');
+}
+putchar('\n');
+return (0);
+}
